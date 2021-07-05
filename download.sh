f981ef1daa8a89399e48839470510fc5aefcf325
anooj-gandham <63911217+anooj-gandham@users.noreply.github.com>
2021-07-05 22:51:58 +0530

    Delete download.sh

--- download.sh
+++
@@ -1,5 +0,0 @@
-cd ../newBlogs
-wget --no-check-certificate 'https://drive.google.com/uc?id=1TvEX0PZN1fnOAw3S_zzl0wzX9UeWfqoz&export=download' -r -A 'uc*' -e robots=off -nd
-mv 'uc?id=1TvEX0PZN1fnOAw3S_zzl0wzX9UeWfqoz&export=download' new.json
-ls
-cd ../typesense-instantsearch-demo
